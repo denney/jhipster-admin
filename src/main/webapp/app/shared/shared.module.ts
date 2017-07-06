e605157e3bd55f9d09d0ac8a1d8d6d609dfdd2b2
dst <695493589@qq.com>
2017-07-07 00:09:38 +0800

    Initial commit

---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,56 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import { CookieService } from 'angular2-cookie/services/cookies.service';
+import {
+    JhipsterAdminSharedLibsModule,
+    JhipsterAdminSharedCommonModule,
+    CSRFService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    Principal,
+    HasAnyAuthorityDirective,
+    JhiSocialComponent,
+    SocialService,
+    JhiLoginModalComponent
+} from './';
+
+@NgModule({
+    imports: [
+        JhipsterAdminSharedLibsModule,
+        JhipsterAdminSharedCommonModule
+    ],
+    declarations: [
+        JhiSocialComponent,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        CookieService,
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        AuthServerProvider,
+        SocialService,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        JhipsterAdminSharedCommonModule,
+        JhiSocialComponent,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class JhipsterAdminSharedModule {}
